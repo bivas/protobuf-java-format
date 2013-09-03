7b02d589224854583014773497f46cb7b579c2e4
Dmitry Kulikov <kulikov.dm@gmail.com>
2013-09-03 13:56:14 +0400

    Delete compile_proto.sh

--- compile_proto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PROTOPATH=$(cd "$(dirname "$0")/src/test/resources"; pwd)
-PROTOFILES=`find $PROTOPATH -iname *.proto | xargs $1`
-
-rm -rf $PROTOPATH/../java/*
-protoc -I=$PROTOPATH --java_out=$PROTOPATH/../java/ $PROTOFILES
-echo "ok"
